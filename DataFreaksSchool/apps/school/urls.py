ff6950cdc60f3ebf5c105191e5c511ea0f4c82c6
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-03 18:45:25 -0500

    setting students details in teacher view

--- DataFreaksSchool/apps/school/urls.py
+++ DataFreaksSchool/apps/school/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .views import school, student, teacher
 
 urlpatterns = [
-    path('teacher/students/', login_required(teacher.StudentListView.as_view()), name='students_list'),
+    path('<int:pk>/', login_required(teacher.DetailCourseView.as_view()), name='students_list'),
     path('teacher/students/<int:pk>/', login_required(teacher.NoteView.as_view()), name='student_note'),
     path('teacher/', login_required(teacher.ListCourseView.as_view()), name='teacher_list'),
 
--- DataFreaksSchool/apps/school/views/teacher.py
+++ DataFreaksSchool/apps/school/views/teacher.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from django.utils.decorators import method_decorator
 from django.contrib.auth import login
 from django.shortcuts import redirect
@@ -21,8 +21,7 @@ class TeacherSignUpView(CreateView):
 
 
 class ListCourseView(ListView):
-    print("TEACHERLIST!!!")
-    model = Teacher
+    model = Course
     template_name = 'school/list_course.html'
     context_object_name = 'courses'
 
@@ -32,34 +31,25 @@ class ListCourseView(ListView):
         return Course.objects.filter(teacher=self.request.user.id)
 
 
-class StudentListView(ListView):
-    model = Teacher
+class DetailCourseView(DetailView):
+    model = Course
     template_name = 'school/student_list.html'
     context_object_name = 'students'
-    success_url = reverse_lazy('school:students_list')
 
     def get_queryset(self):
-        print(self.request.user, type(self.request.user.id))
-        cor = Course.objects.filter(teacher=self.request.user.id)
-        print(cor)
-        print(Note.student)
-        for student in Note.objects.filter(course=cor[0]):
-            print(student.student)
-        students = Note.objects.filter(course=cor[0])
-        print(dir(students))
-        print(students.all())
-        return students.all()
+        print(self.request.user.id, type(self.request.user.id))
+        print(Course.objects.filter(teacher=self.request.user.id))
+        return Course.objects.filter(teacher=self.request.user.id)
 
-
-class NoteView(CreateView):
+class NoteView(UpdateView):
     model = Note
     form_class = NoteForm
     template_name = 'school/note_edit.html'
     success_url = reverse_lazy('school:student_note')
 
 
-class NoteUpdateView(CreateView):
-    model = Note
-    form_class = NoteForm
-    template_name = 'school/note_edit.html'
-    success_url = reverse_lazy('school:student_note')
+# class NoteUpdateView(CreateView):
+#     model = Note
+#     form_class = NoteForm
+#     template_name = 'school/note_edit.html'
+#     success_url = reverse_lazy('school:student_note')
